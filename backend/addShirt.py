8465d7f2e082a91a28979afe4a828772b4b0b1da
Yashank Bhola <64089101+yashankxy@users.noreply.github.com>
2024-01-27 20:57:20 -0500

    improved valr

--- backend/addShirt.py
+++ backend/addShirt.py
@@ -3,29 +3,29 @@ import cvzone
 import os
 
 # def addShirt(frame, landmarks, imgIndex, rightCounter, leftCounter, shirtList, shirtDir, ratio, shirtRatio, speed, rightButtonImg, leftButtonImg):
-def addShirt(frame, lmList, imageNumber, counterRight, counterLeft, listShirts, shirtFolderPath, fixedRatio, shirtRatioHeightWidth, selectionSpeed, imgButtonRight, imgButtonLeft):
+def addShirt(frame, lm_List, imageNumber, counterRight, counterLeft, listShirts, shirtFolderPath, fixedRatio, shirtRatioHeightWidth, selectionSpeed, imgButtonRight, imgButtonLeft):
     # center = bboxInfo["center"]
-    lm11 = lmList[11][1:3]
-    lm12 = lmList[12][1:3]
-    imgShirt = cv2.imread(os.path.join(shirtFolderPath, listShirts[imageNumber]), cv2.IMREAD_UNCHANGED)
+    landmark11 = lm_List[11][1:3]
+    landmark12 = lm_List[12][1:3]
+    img_Shirt = cv2.imread(os.path.join(shirtFolderPath, listShirts[imageNumber]), cv2.IMREAD_UNCHANGED)
 
-    widthOfShirt = int((lm11[0] - lm12[0]) * fixedRatio)
-    print(widthOfShirt)
+    width_of_Shirt = int((landmark11[0] - landmark12[0]) * fixedRatio)
+    print(width_of_Shirt)
 
-    imgShirt = cv2.resize(imgShirt, (widthOfShirt, int(widthOfShirt * shirtRatioHeightWidth)))
+    img_Shirt = cv2.resize(img_Shirt, (width_of_Shirt, int(width_of_Shirt * shirtRatioHeightWidth)))
 
-    currentScale = (lm11[0] - lm12[0]) / 190
-    offset = int(44 * currentScale), int(48 * currentScale)
+    current_Scale = (landmark11[0] - landmark12[0]) / 190
+    offset = int(44 * current_Scale), int(48 * current_Scale)
 
     try:
-        frame = cvzone.overlayPNG(frame, imgShirt, (lm12[0] - offset[0], lm12[1] - offset[1]))
+        frame = cvzone.overlayPNG(frame, img_Shirt, (landmark12[0] - offset[0], landmark12[1] - offset[1]))
     except:
         pass
 
     # img = cvzone.overlayPNG(img, imgButtonRight, (1074, 293))
     # img = cvzone.overlayPNG(img, imgButtonLeft, (72, 293))
 
-    if lmList[16][1] < 300:
+    if lm_List[16][1] < 300:
         counterRight += 1
         cv2.ellipse(frame, (139, 360), (66, 66), 0, 0,
                     counterRight * selectionSpeed, (0, 255, 0), 20)
@@ -33,7 +33,7 @@ def addShirt(frame, lmList, imageNumber, counterRight, counterLeft, listShirts, 
             counterRight = 0
             if imageNumber < len(listShirts) - 1:
                 imageNumber += 1
-    elif lmList[15][1] > 900:
+    elif lm_List[15][1] > 900:
         counterLeft += 1
         cv2.ellipse(frame, (1138, 360), (66, 66), 0, 0,
                     counterLeft * selectionSpeed, (0, 255, 0), 20)
